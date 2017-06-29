8465839e5a5e9c791ea3652a8044ccfeb2bec2bd
phunghx_2019 <phunghx@gmail.com>
2017-06-30 02:51:13 +0900

    testing command

--- testing.sh
+++ testing.sh
@@ -1,3 +1,5 @@
-th test_v2.lua -datasave dataTest -datainput data/shapeTest
-python findFaces.py --image_folder preprocessing/data/testImage --save testPairs.pkl
+testshape=$1
+testImage=$2
+th test_v2.lua -datasave dataTest -datainput $testshape	#data/shapeTest
+python findFaces.py --image_folder $testImage --save testPairs.pkl  #preprocessing/data/testImage
 python predict_test_66_7_percent.py

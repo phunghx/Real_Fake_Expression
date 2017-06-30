999005104dcbe862974b93d2c608b1ac53464c51
phunghx_2019 <phunghx@gmail.com>
2017-06-30 18:27:29 +0900

    xgboost

---
+++ training/run_val.sh
@@ -0,0 +1,7 @@
+datastr=$1
+th run_val.lua -facial anger -data $datastr &
+th run_val.lua -facial contentment -data $datastr &
+th run_val.lua -facial disgust -data $datastr &
+th run_val.lua -facial happy -data $datastr &
+th run_val.lua -facial sadness -data $datastr &
+th run_val.lua -facial surprise  -data $datastr 

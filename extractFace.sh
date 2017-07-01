a45353258cce176f8aa988a2700dff091ed78ce9
phunghx_2019 <phunghx@gmail.com>
2017-07-01 14:25:08 +0900

    testing

--- extractFace.sh
+++ extractFace.sh
@@ -1,7 +1,7 @@
 testdata=$1
 cd preprocessing
 python face_box_v2.py --video_folder $testdata --save ./testImage
-th generateTesting.lua -input ./testImage -output ../data/shapeTest
+th generateTesting.lua -input ../data/testImage -output ../data/shapeTest
 cd ..
 
 
